2f8218e36c6d36c6932c0f3d0437186494990f4d
Sameeksha04 <92214681+Sameeksha04@users.noreply.github.com>
2025-05-18 15:11:14 +0530

    Add files via upload

---
+++ Assignment4/Assignment4_2.py
@@ -0,0 +1,8 @@
+multiplication = lambda a,b : a*b
+print("enter two numbers...")
+value1=int(input())
+value2=int(input())
+
+ret = multiplication(value1,value2)
+
+print("multiplication is : ",ret)
